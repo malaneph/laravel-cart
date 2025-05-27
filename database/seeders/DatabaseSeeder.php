29da5a385817dfeeb477aae259c75de79b9699c0
malaneph <malansand@gmail.com>
2025-05-28 00:37:38 +0700

    categories: migration, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => 'secret'
         ]);
+
+        foreach (['легкий', 'хрупкий', 'тяжелый'] as $category_name) {
+            Category::create(['name' => $category_name]);
+        }
     }
 }
