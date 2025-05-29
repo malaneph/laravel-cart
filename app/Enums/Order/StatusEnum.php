24309df1db2078d661fbd29b383f7b24edcd78d2
malaneph <malansand@gmail.com>
2025-05-29 19:25:06 +0700

    statusenum values as array

--- app/Enums/Order/StatusEnum.php
+++ app/Enums/Order/StatusEnum.php
@@ -7,4 +7,26 @@ enum StatusEnum: int
     case IN_PROGRESS = 1;
     case SHIPPED = 2;
     case COMPLETED = 3;
+
+    public function description(): string
+    {
+        return match ($this->value) {
+            self::IN_PROGRESS->value => __('Собирается'),
+            self::SHIPPED->value => __('Отправлен'),
+            self::COMPLETED->value => __('Выполнен')
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_map(
+            static function ($case) {
+                return [
+                    'value' => $case->value,
+                    'description' => $case->description()
+                ];
+            },
+            self::cases()
+        );
+    }
 }
